af0b6bec3ce736088c28a36ac4e1c8f95e10043e
Thomas Orozco <thomas@orozco.fr>
2016-02-01 17:49:27 +0100

    Get rid of --active-leader

--- 1.5/run-database.sh
+++ 1.5/run-database.sh
@@ -6,14 +6,18 @@ set -o pipefail
 . /usr/bin/utilities.sh
 
 
+# Environment variables that might be provided by the environmen
+
+: ${PUBLISH_HOST:=""}       # Host this container's ports may be published on.
+: ${PUBLISH_PORT_9300:=""}  # Port this container's port 9300 may be published on.
+: ${PUBLISH_PORT_443:=""}   # Similar to above.
+
 # File name constants
 ES_CLUSTER_NAME_FILE="${DATA_DIRECTORY}/cluster-name"
 ES_CLUSTER_HOSTS_FILE="${DATA_DIRECTORY}/cluster-hosts"
 
 
-sed "s:SSL_DIRECTORY:${SSL_DIRECTORY}:g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-function elastic_initialize_conf_dir () {
+function elastic_runtime_conf () {
   # Load cluster name
   if [[ -f "${ES_CLUSTER_NAME_FILE}" ]]; then
     CLUSTER_NAME="$(cat "${ES_CLUSTER_NAME_FILE}")"
@@ -30,52 +34,75 @@ function elastic_initialize_conf_dir () {
   es_config="/elasticsearch/config/elasticsearch.yml"
   cp "${es_config}"{.template,}
 
-  sed -i "s/__CLUSTER_NAME__/${CLUSTER_NAME}/g"             "${es_config}"
-  sed -i "s/__CLUSTER_HOSTS__/${CLUSTER_HOSTS}/g"           "${es_config}"
+  sed -i "s/__CLUSTER_NAME__/${CLUSTER_NAME}/g"   "${es_config}"
+  sed -i "s/__CLUSTER_HOSTS__/${CLUSTER_HOSTS}/g" "${es_config}"
 
-  # TODO - We probably should not expose those to always be available, or
-  # expect them to be provided under this name.
-  sed -i "s/__NODE_PUBLISH_HOST__/${NODE_PUBLISH_HOST}/g"   "${es_config}"
-  sed -i "s/__NODE_PUBLISH_PORT__/${NODE_PUBLISH_PORT}/g"   "${es_config}"
+
+  # If we have a publish host, write it in the config file. Otherwise,
+  # remove that section altogether. Same for port.
+
+  if [[ -n "${PUBLISH_HOST}"  ]]; then
+    sed -i "s/__NODE_PUBLISH_HOST__/${PUBLISH_HOST}/g" "${es_config}"
+  else
+    sed -i "/__NODE_PUBLISH_HOST__/d" "${es_config}"
+  fi
+
+  if [[ -n "${PUBLISH_PORT_9300}"  ]]; then
+    sed -i "s/__NODE_PUBLISH_PORT__/${PUBLISH_PORT_9300}/g" "${es_config}"
+  else
+    sed -i "/__NODE_PUBLISH_PORT__/d" "${es_config}"
+  fi
 }
 
 
-if [[ "$1" == "--initialize" ]]; then
-  # Nginx SSL Setup
-  htpasswd -b -c "${DATA_DIRECTORY}/auth_basic.htpasswd" "${USERNAME:-aptible}" "$PASSPHRASE"
+function nginx_runtime_conf () {
+  sed "s:__SSL_DIRECTORY__:${SSL_DIRECTORY}:g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+}
+
+
+function nginx_init_htpasswd () {
+  local user="$1"
+  local pass="$2"
+
+  htpasswd -b -c "${DATA_DIRECTORY}/auth_basic.htpasswd" "$user" "$pass"
+}
+
+
+function nginx_init_certs () {
+  # TODO - Run ES as separate user to prevent access to SSL certs.
   if [ -n "$SSL_CERTIFICATE" ] && [ -n "$SSL_KEY" ]; then
     echo "$SSL_CERTIFICATE" > "$SSL_DIRECTORY"/server.crt
     echo "$SSL_KEY" > "$SSL_DIRECTORY"/server.key
     chmod og-rwx "$SSL_DIRECTORY"/server.key
   fi
+}
 
-  # TODO - Run ES as separate user to prevent access to SSL certs.
 
-  # Discover cluster name, load up master host.
-  echo "Initializing cluster name"
+if [[ "$1" == "--initialize" ]]; then
+  set -o xtrace
+  nginx_init_htpasswd "${USERNAME:-aptible}" "$PASSPHRASE"
+  nginx_init_certs
+
   CLUSTER_NAME="es-$(pwgen -s 10)"
   echo "${CLUSTER_NAME}" > "${ES_CLUSTER_NAME_FILE}"
 
-elif [[ "$1" == "--activate-leader" ]]; then
-  # TODO - Document that this *needs* to have access to the master volumes.
-  # Export htaccess for slave to replicate.
-  echo "ELASTIC_REPLICATION_HTACCESS='$(cat "${DATA_DIRECTORY}/auth_basic.htpasswd")'"
-
 elif [[ "$1" == "--initialize-from" ]]; then
-  [ -z "$2" ] && echo "docker run aptible/elasticsearch --initialize-from https://..." && exit
+  [ -z "$2" ] && echo "docker run aptible/elasticsearch --initialize-from https://..." && exit 1
+  set -o xtrace
 
-  # TODO - Certs
+  # Parse out username and password
+  parse_url "$2"
 
-  echo "${ELASTIC_REPLICATION_HTACCESS}" > "${DATA_DIRECTORY}/auth_basic.htpasswd"
+  nginx_init_htpasswd "$user" "$password"
+  nginx_init_certs
 
-  # Get the cluster name and nodes, store them
-  # https://www.elastic.co/guide/en/elasticsearch/reference/1.5/cluster-nodes-info.html
-  # TODO - Do we want to keep --insecure here?
+  # Fetch cluster settings from master - ${2} is probably /-terminated, so we might end
+  # up with a // in here, but that's fine - better safe than sorry.
   es_settings="$(curl --insecure --silent "${2}/_nodes" | extract_es_settings.py)"
   eval "$es_settings"
 
   echo "${CLUSTER_NAME}" > "${DATA_DIRECTORY}/cluster-name"
-  echo "${CLUSTER_HOSTS}" > "${DATA_DIRECTORY}/cluster-hosts" # TODO - Do we need to update this periodically to register new hosts?
+  echo "${CLUSTER_HOSTS}" > "${DATA_DIRECTORY}/cluster-hosts"
 
 elif [[ "$1" == "--client" ]]; then
   echo "This image does not support the --client option. Use curl instead." && exit 1
@@ -90,12 +117,21 @@ elif [[ "$1" == "--restore" ]]; then
   parse_url "$2"
   elasticdump --bulk=true --input=$ --output=${protocol:-https}"://"$user":"$password"@"$host":"${port:-80}""
 
+elif [[ "$1" == "--info" ]]; then
+  if [[ -z "$USERNAME" ]] || [[ -z "$PASSPHRASE" ]] || [[ -z "$PUBLISH_HOST" ]] || [[ -z "$PUBLISH_PORT_443" ]]; then
+    echo "One of PUBLISH_HOST, USERNAME, PASSPHRASE, PUBLISH_PORT_443 is missing from the environment"
+    exit 1
+  fi
+  echo "CONNECTION_URL=https://${USERNAME}:${PASSPHRASE}@${PUBLISH_HOST}:${PUBLISH_PORT_443}/"
+
 elif [[ "$1" == "--readonly" ]]; then
-  elastic_initialize_conf_dir
+  elastic_runtime_conf
+  nginx_runtime_conf
   READONLY=1 /usr/sbin/nginx-wrapper
 
 else
-  elastic_initialize_conf_dir
+  elastic_runtime_conf
+  nginx_runtime_conf
   /usr/sbin/nginx-wrapper
 
 fi
--- test.sh
+++ test.sh
@@ -9,23 +9,25 @@ IMG="quay.io/aptible/elasticsearch:$VERSION"
 
 DATA="/tmp/docker-elasticsearch/data"
 
+# TODO - Be more dynamic
+DOCKER_VM_HOST="192.168.99.100"
+
+ES_USERNAME="test"
+ES_PASSPHRASE="testpass"
+
+
 ES_A_CONTAINER="es-a"
 ES_A_HTTPS_PORT=4431
 ES_A_TRANSPORT_PORT=19301
+ES_A_URL="https://$ES_USERNAME:$ES_PASSPHRASE@$DOCKER_VM_HOST:$ES_A_HTTPS_PORT/"
 
 ES_B_CONTAINER="es-b"
 ES_B_HTTPS_PORT=4432
 ES_B_TRANSPORT_PORT=19302
+ES_B_URL="https://$ES_USERNAME:$ES_PASSPHRASE@$DOCKER_VM_HOST:$ES_B_HTTPS_PORT/"
 
 ES_HTTPS_PORT=443
 ES_TRANSPORT_PORT=9300
-
-DOCKER_VM_HOST="192.168.99.100"  # TODO - Be more dynamic
-
-
-USERNAME="test"
-PASSPHRASE="testpass"
-
 
 
 # Cleanup
@@ -36,39 +38,56 @@ docker-machine ssh docker-vm sudo rm -rf "${DATA}"
 make 1.5
 
 
+echo "Initializing ${ES_A_CONTAINER}"
+docker run -it --rm \
+  -v "${DATA}/${ES_A_CONTAINER}:/var/db" \
+  -e USERNAME="$ES_USERNAME" -e PASSPHRASE="$ES_PASSPHRASE" \
+  "$IMG" --initialize
+
+
 echo "Running ${ES_A_CONTAINER}"
 docker run -d --name "$ES_A_CONTAINER" \
   -p "$ES_A_HTTPS_PORT:$ES_HTTPS_PORT" -p "$ES_A_TRANSPORT_PORT:$ES_TRANSPORT_PORT" \
-  -e NODE_PUBLISH_HOST="$DOCKER_VM_HOST" -e NODE_PUBLISH_PORT="$ES_A_TRANSPORT_PORT" \
-  -e USERNAME="$USERNAME" -e PASSPHRASE="$PASSPHRASE" \
-  --entrypoint bash \
   -v "${DATA}/$ES_A_CONTAINER:/var/db" \
-  "${IMG}" \
-  -c "run-database.sh --initialize && run-database.sh"
+  -e PUBLISH_HOST="$DOCKER_VM_HOST" -e PUBLISH_PORT_9300="$ES_A_TRANSPORT_PORT" \
+  "${IMG}"
 
 
 echo "Waiting for ${ES_A_CONTAINER} to come online"
-until curl --insecure --silent --fail > /dev/null "https://$USERNAME:$PASSPHRASE@$DOCKER_VM_HOST:$ES_A_HTTPS_PORT"; do sleep 0.5; done
+until curl --insecure --silent --fail > /dev/null "$ES_A_URL"; do sleep 0.5; done
 
 
-echo "Extracting init data from ${ES_A_CONTAINER}"
-eval "$(docker run -it -v "${DATA}/${ES_A_CONTAINER}:/var/db" --rm "$IMG" --activate-leader | dos2unix)"
+echo "Initializing ${ES_B_CONTAINER} (from ${ES_A_CONTAINER})"
+docker run -it --rm \
+  -v "${DATA}/$ES_B_CONTAINER:/var/db" \
+  -e USERNAME="$ES_USERNAME" -e PASSPHRASE="$ES_PASSPHRASE" \
+  "$IMG" --initialize-from "$ES_A_URL"
 
 
-echo "Running ${ES_B_CONTAINER} (from ${ES_A_CONTAINER})"
+echo "Running ${ES_B_CONTAINER}"
 docker run -d --name "$ES_B_CONTAINER" \
   -p "$ES_B_HTTPS_PORT:$ES_HTTPS_PORT" -p "$ES_B_TRANSPORT_PORT:$ES_TRANSPORT_PORT" \
-  -e NODE_PUBLISH_HOST="$DOCKER_VM_HOST" -e NODE_PUBLISH_PORT="$ES_B_TRANSPORT_PORT" \
-  -e "ELASTIC_REPLICATION_HTACCESS=$ELASTIC_REPLICATION_HTACCESS" \
-  --entrypoint bash \
-  -v "${DATA}/$ES_A_CONTAINER:/var/db" \
-  "$IMG" \
-  -c "run-database.sh --initialize-from https://$USERNAME:$PASSPHRASE@$DOCKER_VM_HOST:$ES_A_HTTPS_PORT/ && run-database.sh"
+  -v "${DATA}/$ES_B_CONTAINER:/var/db" \
+  -e PUBLISH_HOST="$DOCKER_VM_HOST" -e PUBLISH_PORT_9300="$ES_B_TRANSPORT_PORT" \
+  "$IMG"
 
 
 echo "Waiting for ${ES_B_CONTAINER} to come online"
-until curl --insecure --silent --fail > /dev/null "https://$USERNAME:$PASSPHRASE@$DOCKER_VM_HOST:$ES_B_HTTPS_PORT"; do sleep 0.5; done
+until curl --insecure --silent --fail > /dev/null "$ES_B_URL"; do sleep 0.5; done
 
 
 echo "Running tests on ${ES_A_CONTAINER}, ${ES_B_CONTAINER}"
+curl --silent --insecure "${ES_A_URL}/_cluster/health" | python -c 'import json, sys; assert json.load(sys.stdin)["number_of_nodes"] == 2'
 
+
+# Create some data on ES-A!
+curl --insecure --fail -XPUT "${ES_A_URL}customer/?pretty=true"
+curl --insecure --fail -XPUT "${ES_A_URL}/customer/external/1?pretty=true" -d '{
+  "name": "John Doe"
+}'
+
+# Check out the data on ES-B!
+curl --silent --insecure --fail -XGET "${ES_B_URL}/customer/external/1?pretty"
+
+
+echo "Test OK!"
