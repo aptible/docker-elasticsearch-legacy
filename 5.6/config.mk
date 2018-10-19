136b98bab22f82db9114553d35ee592e24d4146f
Alex Kubacki <alex@aptible.com>
2018-10-19 14:45:49 -0400

    Update versions

--- 5.6/config.mk
+++ 5.6/config.mk
@@ -1,5 +1,5 @@
-export ES_VERSION = 5.6.11
-export ES_SHA1SUM = 60de273a1661f8c5f37b4e974d337c5962f2ded5
+export ES_VERSION = 5.6.12
+export ES_SHA1SUM = bc57fb5f089d7859ad2db5b10dc7561c711fd419
 export ES_BACKUP_PLUGIN = repository-s3
 export ES_USER = elasticsearch
 export ES_GROUP = elasticsearch
