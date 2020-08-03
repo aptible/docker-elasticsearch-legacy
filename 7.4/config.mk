a6bfd69d7a35c7c607609c2312080e721177dd82
root <root@ubuntu.alexkubcaki.com>
2020-08-03 19:20:13 +0000

    Update to same hash format ES provides on their download page.

--- 7.4/config.mk
+++ 7.4/config.mk
@@ -1,3 +1,3 @@
 export ES_VERSION = 7.4.2
 export DOWNLOAD_URL = https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$(ES_VERSION)-linux-x86_64.tar.gz
-export ES_SHA1SUM = 74ffbd80e31cdb29ac74e41798f2ba42d5b52d89
+export ES_SHA512SUM = 64cc3e77f4271a5477c8c979fa48728d96890cad68b0daac6566f4dda25b4d4a80784eaafeaa874a6b434c034fcacb8a5751fdb445919191bae4aa4958c793b7
--- 7.5/config.mk
+++ 7.5/config.mk
@@ -1,3 +1,3 @@
-export ES_VERSION = 7.5.1
+export ES_VERSION = 7.5.2
 export DOWNLOAD_URL = https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$(ES_VERSION)-linux-x86_64.tar.gz
-export ES_SHA1SUM = 8c34bc04d480ab37da128e15c0ca86f0a64f58e5
+export ES_SHA512SUM = a9dfc062f010a73a8774745730465e768d58cf418579f0aef0b0032e6be49285a9077be3d08b729679d1895b97ced3a1b061b075c167c15e6faf08267a179e52
